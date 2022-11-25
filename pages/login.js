28cced5ce5fa8af08340743b864e877584c89d58
hassan asadollahi <hasan.asadolahi2@gmail.com>
2022-11-25 18:54:53 +0330

    admin-taroneh-6

--- pages/login.js
+++ pages/login.js
@@ -1,6 +1,8 @@
 import Login from '@/components/login/login';
+import { NEXT_URL } from '../config';
 
 export default function LoginPage() {
+  console.log(NEXT_URL)
   return (
     <div className="w-full min-h-screen grid place-items-center">
       <Login />
