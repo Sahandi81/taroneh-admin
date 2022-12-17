cd268b39f81db7cbbfa4dc6d1cb70551be51f638
hassan asadollahi <hasan.asadolahi2@gmail.com>
2022-12-17 13:44:02 +0330

    taroneh-14

--- config/index.js
+++ config/index.js
@@ -1,3 +1,3 @@
 export const API_URL =   'https://api.taroneh.ir' ;
-export const NEXT_URL =  process.env.NEXT_PUBLIC_NEXT_URL ;
+export const NEXT_URL =  process.env.NEXT_PUBLIC_FRONTEND_URL ;
 // 'http://localhost:3001' || 
